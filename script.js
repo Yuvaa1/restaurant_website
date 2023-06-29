b1f4db709b67d91240900359a01300f05f2bec99
Yuvaa1 <yuvasrigk47@gmail.com>
2023-06-29 17:49:09 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// Get the button element
+var bookTableButton = document.querySelector('.btn');
+
+// Add event listener for the button click
+bookTableButton.addEventListener('click', function() {
+  // Display a confirmation message
+  alert('Table booked successfully!');
+});
